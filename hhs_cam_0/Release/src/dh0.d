48d1be23b686b6ac900d92c39114ee489e57d995
Luca van Straaten <lucavanstraaten@icloud.com>
2022-08-31 17:05:05 +0200

    initial commit, fresh program

---
+++ hhs_cam_0/Release/src/dh0.d
@@ -0,0 +1,6 @@
+src/dh0.o: ../src/dh0.cpp /usr/include/opencv2/opencv.hpp ../src/dh0.h \
+ ../src/GxIAPI.h ../src/GxImageProc.hpp
+/usr/include/opencv2/opencv.hpp:
+../src/dh0.h:
+../src/GxIAPI.h:
+../src/GxImageProc.hpp:
---
+++ hhs_cam_0/Release/src/hhs_cam.d
@@ -0,0 +1,8 @@
+src/hhs_cam.o: ../src/hhs_cam.cpp /usr/include/opencv2/opencv.hpp \
+ /usr/include/opencv2/highgui.hpp ../src/dh0.h ../src/GxIAPI.h \
+ ../src/GxImageProc.hpp
+/usr/include/opencv2/opencv.hpp:
+/usr/include/opencv2/highgui.hpp:
+../src/dh0.h:
+../src/GxIAPI.h:
+../src/GxImageProc.hpp:
