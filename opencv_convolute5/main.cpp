1f7d5dae18a4c2e472c442f388da1e2dc9e5553f
lucanatorvs <lucavanstraaten@icloud.com>
2022-11-17 17:47:15 +0100

    work on report

--- opencv_convolute5/main.cpp
+++ opencv_convolute5/main.cpp
@@ -10,36 +10,35 @@ using namespace cv;
 //int* from original assignment replaced with pointer to the kernel instead, because that makes more sense
 void convolve5 (Mat* inputImg, Mat* outImg, int (*kernel5)[5][5]) { 
 	//I assume the mat is CV_8UC1 since I want to process BGR channels individually
-	Size s = inputImg -> size();
-	const uint8_t kernel_size = 5; //todo: replace with sizeof
-	uint8_t out[s.height][s.width];
-	int x ,y, h, w, i, j, sum;
+	Size s = inputImg -> size(); // get size of image
+	const uint8_t kernel_size = 5; // todo: replace with sizeof
+	uint8_t out[s.height][s.width]; // create output array
+	int x ,y, h, w, i, j, sum; // declare variables
 	
-	std::cout << "Input type was : " << inputImg->type() << std::endl;
+	std::cout << "Input type was : " << inputImg->type() << std::endl; //debug
   
-	for (h=0; h<s.height; h++) {
-		for(w=0; w<s.width; w++){
-			sum = 0;
-			for(i=0 ;i<kernel_size; i++ ){
-				for(j=0; j<kernel_size; j++){
-					y=h-i+1; x=w-j+1;
-					//border of two pixels
+	for (h=0; h<s.height; h++) { // for each row
+		for(w=0; w<s.width; w++){ // for each column
+			sum = 0; // reset sum
+			for(i=0 ;i<kernel_size; i++ ){ // for each kernel row
+				for(j=0; j<kernel_size; j++){ // for each kernel column
+					y=h-i+1; x=w-j+1; // calculate the position of the pixel in the image
+					// if the pixel is outside the image, set it to the border
 					if(y<0) y=0;
-					if(y>s.height-2) y=s.height-2;
+					if(y>s.height-2) y=s.height-2; 
 					if(x<0) x=0;
 					if(x>s.width-2) x=s.width-2;
-					//sum+=kernel[i][j]*inputImg->at<uint8_t>(y,x);
-					sum += ((*kernel5)[i][j]) * inputImg->at<uint8_t>(y,x);
+					sum += ((*kernel5)[i][j]) * inputImg->at<uint8_t>(y,x); // add the product of the kernel and the pixel to the sum
 				}
 			}
 			sum /= kernel_size*kernel_size; //divide the result of the pixel by 5^2
-			if(sum<0) sum=0;
-			if(sum>255) sum=255;
-			std::memcpy(outImg->data, out, s.height*s.width*sizeof(uint8_t));
-			out[h][w]=(uint8_t)sum;
+			if(sum<0) sum=0; // if the result is negative, set it to 0
+			if(sum>255) sum=255; // if the result is greater than 255, set it to 255
+			std::memcpy(outImg->data, out, s.height*s.width*sizeof(uint8_t)); // copy the result to the output array
+			out[h][w]=(uint8_t)sum; // set the result to the output array
 		}
 	}
-};
+}
 
 
 int main() {
